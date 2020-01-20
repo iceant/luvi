ae3df2e5a122e5ced22f6c3e1616bd6fc5d9a6d1
Chen Peng <pizer.chen@gmail.com>
2020-01-20 15:26:09 +0800

    update sqlite3.cmake for deps/lsqlite3

--- deps/sqlite3.cmake
+++ deps/sqlite3.cmake
@@ -1,8 +1,13 @@
 set(LUA_SQLITE3_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/lsqlite3)
+set(LUA_SQLITE3_LIB lua_sqlite3)
+
+include_directories(deps/lsqlite3)
 
 add_library(lua_sqlite3 
     ${LUA_SQLITE3_DIR}/sqlite3.c
     ${LUA_SQLITE3_DIR}/lsqlite3.c
 )
+
+set_target_properties(lua_sqlite3 PROPERTIES COMPILE_FLAGS -DLUA_LIB)
 add_definitions(-DWITH_SQLITE)
 set(EXTRA_LIBS ${EXTRA_LIBS} lua_sqlite3)
